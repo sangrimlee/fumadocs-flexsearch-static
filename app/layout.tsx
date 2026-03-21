2cb8f5e66fe7cc24d446fe54a4990e54dcb7030e
이상림 <contact@sangrim.dev>
2026-03-21 21:52:27 +0900

    feat: add default search dialog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import "./global.css";
+
+import { RootProvider } from "fumadocs-ui/provider/next";
+import { Inter } from "next/font/google";
+import DefaultSearchDialog from "@/components/search";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider search={{ SearchDialog: DefaultSearchDialog }}>
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
